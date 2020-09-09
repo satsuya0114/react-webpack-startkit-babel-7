b94f9af109ae63043ba30c85fa66d073c94aa6c3
satsuya0114 <satsuya0114@gmail.com>
2020-09-09 11:26:51 +0800

    update component to functional component:

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Button} from 'antd';
 import './Home.less';
 
-export default class Home extends React.PureComponent {
-  render() {
-    return (
-      <div style={{ textAlign: 'center' }} >
-        <h2 className="orange">Hello, This is webpack demo for React</h2>
-        <p>:D</p>
-        <Button type="primary">Primary</Button>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <div style={{ textAlign: 'center' }} >
+      <h2 className="orange">Hello, This is webpack demo for React</h2>
+      <p>:D</p>
+      <Button type="primary">Primary</Button>
+    </div>
+  );
+};
+
+export default Home;
